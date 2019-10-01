281330663688b7520ab12c8e0fe362c58f9bc3e8
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-10-01 13:00:32 -0500

    Updates

---
+++ precache-manifest.cdc5d5f723a0bf8e9c4e4f5b2631a3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d4a7cb3a32a65f0df945390a632d75",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f4aa1496e2e08c2a876b",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "310b64553ffca4f3827f",
+    "url": "/portfolio/static/js/2.c8972d9e.chunk.js"
+  },
+  {
+    "revision": "f4aa1496e2e08c2a876b",
+    "url": "/portfolio/static/js/main.39190a96.chunk.js"
+  },
+  {
+    "revision": "a46b3fd8f34f8691f45e",
+    "url": "/portfolio/static/js/runtime-main.6c1b9ebb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
