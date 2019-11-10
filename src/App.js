fa6d327d7c4452efec73d583ed68e9db0a9b2eee
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-11-09 19:39:33 -0600

    Se modifican estilos estéticos

--- src/App.js
+++ src/App.js
@@ -5,58 +5,57 @@ import Header from './components/Header';
 import Avatar from './components/Avatar';
 import Content from './components/Content';
 
+
+const particlesOpt = {
+	"particles": {
+		"number": {
+			"value": 170
+		},
+		"size": {
+			"value": 1
+		}
+  },
+  
+	"interactivity": {
+		"events": {
+			"onhover": {
+				"enable": true,
+				"mode": "repulse"
+			},
+			"onclick":{
+				"enable":false,
+				"mode":"push"
+			},
+			"modes":{
+				"grab":{
+					"line_linked":{
+						"distance": 100
+					}
+				}
+			}
+			
+		}
+	}	
+}
+
+
 class App extends Component {
+
 
   render(){
   return (
 
-    <div className="App">
+    <div className="App" id= "home">
 		<div className="principal">
 		<header className="App-header">
 		<Header/>
 		</header>
-
-
-			<Particles 
-		        canvasClassName="background"
-                params={{
-	    "particles": {
-	        "number": {
-	            "value": 170
-	        },
-	        "size": {
-	            "value": 1
-	        }
-      },
-      
-	    "interactivity": {
-	        "events": {
-	            "onhover": {
-	                "enable": true,
-	                "mode": "repulse"
-				},
-				"onclick":{
-					"enable":false,
-					"mode":"push"
-				},
-				"modes":{
-					"grab":{
-						"line_linked":{
-							"distance": 100
-						}
-					}
-				}
-				
-	        }
-		}	
-	}} 
-	
-	/>
-
-
-
-
+			
+			
+			<Particles canvasClassName="background" params={particlesOpt}/>
+			<center className = "name"><h2>Karina Cabrera Software Developer</h2></center>
 		</div>
+		
      
 <div id="about" className="cubrir">
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,20 @@ class Header extends Component{
     render(){
         return(
            <div className= "header">
-               <div className= "line" >
-               <a href="#about"><h4 className="options" >About Me</h4></a>
+                <div className= "line" >
+               <a href="#home"><h5 className="options" >Home</h5></a>
                </div>
                <div className= "line" >
-               <a href="#proyects"><h4 className="options" >Proyects</h4></a>
+               <a href="#about"><h5 className="options" >About Me</h5></a>
                </div>
                <div className= "line" >
-               <a href="#contact"><h4 className="options" >Contact</h4></a>
+               <a href="#proyects"><h5 className="options" >Proyects</h5></a>
+               </div>
+               <div className= "line" >
+               <a href="#skills"><h5 className="options" >Skills</h5></a>
+               </div>
+               <div className= "line" >
+               <a href="#contact"><h5 className="options" >Contact</h5></a>
                </div>
 
             </div>
