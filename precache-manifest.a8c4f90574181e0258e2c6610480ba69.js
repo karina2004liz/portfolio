61632b7f2b5158f57870d45e66b374e739b5cdb6
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-11-23 18:39:17 -0600

    Updates

--- precache-manifest.a8c4f90574181e0258e2c6610480ba69.js
+++ precache-manifest.a8c4f90574181e0258e2c6610480ba69.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1b06f28a3da31f2762a80d80d0d75381",
+    "revision": "3ae0606a4276a40c339af1d4d98e260e",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "3b4a222fdaa1185ddf61",
-    "url": "/portfolio/static/css/main.0ccb41d3.chunk.css"
+    "revision": "308ab75b98241135187a",
+    "url": "/portfolio/static/css/main.c820fc1d.chunk.css"
   },
   {
     "revision": "ba31045239b83d85cbf6",
     "url": "/portfolio/static/js/2.31f44024.chunk.js"
   },
   {
-    "revision": "3b4a222fdaa1185ddf61",
+    "revision": "308ab75b98241135187a",
     "url": "/portfolio/static/js/main.120245b9.chunk.js"
   },
   {
@@ -22,6 +22,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "8e7b84e4d7932f731a4f19c9f51d3f41",
     "url": "/portfolio/static/media/about.8e7b84e4.gif"
+  },
+  {
+    "revision": "b7fcdec5398e74fad3cd251f10a74a38",
+    "url": "/portfolio/static/media/aboutmovile.b7fcdec5.gif"
   },
   {
     "revision": "f6d000ca0eff89bfce8ed5d194388690",
@@ -34,6 +38,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e7244a8c2d4385ccbe6e7cf8436433c3",
     "url": "/portfolio/static/media/header.e7244a8c.gif"
+  },
+  {
+    "revision": "dd4d404df2897c7fd74dfde52384b97d",
+    "url": "/portfolio/static/media/headermovile.dd4d404d.gif"
   },
   {
     "revision": "20265182818c5912a11b8816acdff536",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.92a89838284efb2e4338b29b3b3b9b54.js"
+  "/portfolio/precache-manifest.a8c4f90574181e0258e2c6610480ba69.js"
 );
 
 self.addEventListener('message', (event) => {
