2bd1cc695624136be72c3c17fba0a6fb398ed022
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-11-24 18:50:59 -0600

    Se añade enlace para linkedin

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './contact.css';
 import github from './github.png';
-import gmail from './gmail.png';
+import linkedin from './linkedin.png';
 
 class Contact extends Component{
 
@@ -15,7 +15,9 @@ class Contact extends Component{
                 <h3>Karina Lizbeth Cabrera Ruiz</h3>
                 <h3>eng.karina.cabrera@gmail.com</h3>
                 <h3>3317497765</h3>
-                <div className="divLogoGithub" ><a href="https://github.com/karina2004liz" target="_blank" ><img className="logoLink" src={github}/></a></div>
+                <div className="divLogoGithub" ><a href="https://github.com/karina2004liz" target="_blank"><img className="logoLink" src={github}/></a></div>
+                <div className="divLogoGmail" ><a href="https://www.linkedin.com/in/eng-karina-cabrera/" target="_blank" ><img className="logoLink" src={linkedin}/></a></div>
+                
                 
 
             </div>
