783e76b4254d91b165a9bcccb6a869c97f20258d
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-11-24 18:35:00 -0600

    Se añade enlace para repositorio en github

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './contact.css';
+import github from './github.png';
+import gmail from './gmail.png';
 
 class Contact extends Component{
 
@@ -13,6 +15,8 @@ class Contact extends Component{
                 <h3>Karina Lizbeth Cabrera Ruiz</h3>
                 <h3>eng.karina.cabrera@gmail.com</h3>
                 <h3>3317497765</h3>
+                <div className="divLogoGithub" ><a href="https://github.com/karina2004liz" target="_blank" ><img className="logoLink" src={github}/></a></div>
+                
 
             </div>
 
