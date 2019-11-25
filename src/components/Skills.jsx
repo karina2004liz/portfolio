e2cf53df4a3bd82e628eb4c5748f30e2e74f4033
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-11-25 16:58:40 -0600

    Se agregan especificaciones en tools

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -27,8 +27,18 @@ class Skills extends Component{
                 {this.state.data.map(skills =>{
 
                     return(
-                        
+                        <div className="completeCard">
+                            <center>
+                        <div className= "frontCard">
                         <img className="skillsImg" src={skills.img} ></img>
+                        </div>
+                        <div className="backCard">
+                            <h3 className="textCard">{skills.description}</h3>
+                            <h3 className="textCard">{skills.level}</h3>
+                            
+                        </div>
+                        </center>
+                        </div>
                         
                     )
 
--- src/components/data/skills.js
+++ src/components/data/skills.js
@@ -4,51 +4,63 @@ var SKILLS = {
             
             "img": "http://pluspng.com/img-png/logo-javascript-png-java-script-js-logo-format-ai-javascript-logo-vector-png-213.png",
             "description":"JavaScript",
+            "level": "Developer Junior",
 
         },
         {
             "img": "https://cdn.freebiesupply.com/logos/large/2x/html-5-logo-png-transparent.png",
             "description":"HTML",
+            "level": "Developer Junior",
         },
         {
             "img": "https://3.bp.blogspot.com/-oRSUw_TmO9o/XIb61m88fcI/AAAAAAAAIq0/vnxl2zzsXEQsnHI2fH4GjKu_ZT0urRo4wCK4BGAYYCw/s1600/icon%2Bcss%2B3.png",
             "description":"CSS",
+            "level": "Developer Junior",
         },
         {
             "img": "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
             "description":"Reactjs",
+            "level": "Developer Junior",
         },
         {
             "img": "http://pluspng.com/img-png/nodejs-logo-png-node-js-development-296.png",
             "description":"Nodejs",
+            "level": "Developer Junior",
         },
         {
-            "img": "https://firebase.google.com/images/brand-guidelines/logo-standard.png",
+            "img": "https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png",
             "description":"Firebase",
+            "level": "Developer Junior",
         },
         {
-            "img": "https://webassets.mongodb.com/_com_assets/cms/MongoDB_Logo_FullColorBlack_RGB-4td3yuxzjs.png",
+            "img": "https://www.klipfolio.com/sites/default/files/integrations/mongo.png",
             "description":"MongoDB",
+            "level": "Developer Junior",
         },
         {
             "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/306px-ISO_C%2B%2B_Logo.svg.png",
             "description":"C++",
+            "level": "Developer Junior",
         },
         {
             "img": "https://camo.githubusercontent.com/0617f4657fef12e8d16db45b8d73def73144b09f/68747470733a2f2f646576656c6f7065722e6665646f726170726f6a6563742e6f72672f7374617469632f6c6f676f2f6373686172702e706e67",
             "description":"C#",
+            "level": "Developer Junior",
         },
         {
             "img": "https://logos-download.com/wp-content/uploads/2016/10/Java_logo_icon.png",
             "description":"Java",
+            "level": "Developer Junior",
         },
         {
             "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Arduino_Logo.svg/720px-Arduino_Logo.svg.png",
             "description":"Arduino",
+            "level": "Developer Junior",
         },
         {
             "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png",
             "description":"Python",
+            "level": "Developer Junior",
         }
 
     ]
